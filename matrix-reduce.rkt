50365ade697651eaa074cf4f9e886e565a2ded89
Daniel <dlawson@alumni.kzoo.edu>
2012-09-26 08:38:44 -0400

    added a bunch of functions building up to rref, but it doesn't quite work yet

--- matrix-reduce.rkt
+++ matrix-reduce.rkt
@@ -18,22 +18,42 @@
                       (list (scale-first-line Mx))
                       (map (lambda (x)
                              (append '(0) x))
-                           (ref (map cdr (reduce Mx))))
-                      
-                      )
-                     ))
+                           (ref (map cdr (reduce Mx)))))))
 ;(define (subeach Vx Mx) ())
 
+(define (right-column Mx) (map car (map reverse Mx)))
+
+(define (rref Mx) (apply-subtractions (ref Mx)))
+
+(define (apply-subtractions Mx) (if (null? (cdr Mx))
+                                    Mx 
+                                    (append (list 
+                                             (process-subtractions (append (list (car Mx)) 
+                                                                           (reverse (cdr Mx)))) 
+                                             (apply-subtractions (cdr Mx)))))) 
+
+(define (process-subtractions Mx) (if (null? (cdr Mx))
+                                      Mx
+                                      (process-subtractions (append(list(scale-subtract 
+                                                                         (car Mx) (cadr Mx)))
+                                                                   (cddr Mx)))
+                                   ))
+(define (scale-subtract x y) (map - x (map
+                                       (lambda (z) 
+                                             (* z (scale-factor x (place-of-first-non-zero y 0)))) 
+                                           y)
+                              ))
 (define (place-of-first-non-zero L x) (if (= 0 (car L))
                                           (if (null? (cdr L)) null (place-of-first-non-zero (cdr L) (+ x 1)))
                                               x))
 (define (scale-factor V x) (if (= 0 x)
                                (car V)
                                (scale-factor V (- x 1))))
-(place-of-first-non-zero '(0 0 0 0 2 0) 0)
+;(place-of-first-non-zero '(0 0 0 0 2 0) 0)
 
 ;(define (rref Mx)
 ;  ((define (rr Mx) (append (list (subeach (car Mx) (map (lambda (x) (append '(0) x))(rr (map cdr (cdr Mx)))))) (map (lambda (x) (append '(0) x)) (ref (map cdr (cdr Mx))))))
 ;  (rr (ref Mx))))
 
 (ref '((1 3 9 27 5) (1 2 4 8 1) (1 1 1 1 8) (1 4 16 64 10)))
+(rref '((1 3 9 27 5) (1 2 4 8 1) (1 1 1 1 8) (1 4 16 64 10))) ;doesn't crash, but doesn't work yet
