a45e81d1cfd958718528cd85f912128a7f7056a6
Kyle Cronin <kyle.cronin@gmail.com>
2012-09-27 21:53:24 -0400

    spacing out the definitions

--- matrix-reduce.rkt
+++ matrix-reduce.rkt
@@ -2,21 +2,26 @@
 (define (fit-function-to-points points)
   (lambda (x)
     (apply + (map * (power-list x (- (length points) 1)) (coefficients points)))))
+
 (define (coefficients points)
   (right-column (rref (map (lambda (pair-of-points)(makeline pair-of-points (length points))) points))))
+
 (define (makeline my-pair n)
   (append (power-list (car my-pair) n) (cdr my-pair)))
+
 (define (power-list x n)
   (if (= 0 n) (list 1)
       (append (list 1) (map (lambda (y) (* x y)) (power-list x (- n 1))))))
                         
 (define (scale-first-line L) (map  (lambda (x) (/ x (car (car L)))) (car L)))
+
 (define (reduce L) (map (lambda (x) 
                           (map (lambda (y z) (- y z)) x 
                                (map (lambda (zz)
                                       (* zz (car x)))
                                     (scale-first-line L))))
                         (cdr L)))
+
 (define (ref Mx) (if (= 0 (car (car Mx)))
                      (ref (append (cdr Mx) (list (car Mx))))
                      (if (null? (cdr Mx))  (list (scale-first-line Mx))
@@ -45,14 +50,17 @@
                                                                (car Mx) (cadr Mx)))
                                                              (cddr Mx)))
                                    ))
+
 (define (scale-subtract x y) (map - x (map
                                        (lambda (z) 
                                              (* z (scale-factor x (place-of-first-non-zero y 0)))) 
                                            y)
                               ))
+
 (define (place-of-first-non-zero L x) (if (= 0 (car L))
                                           (if (null? (cdr L)) null (place-of-first-non-zero (cdr L) (+ x 1)))
                                               x))
+
 (define (scale-factor V x) (if (= 0 x)
                                (car V)
                                (scale-factor (cdr V) (- x 1)))) 
